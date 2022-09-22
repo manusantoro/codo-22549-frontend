2f6627879794358b9dde2989ef07e93435def1dc
manuel santoro <manuusantoro@gmail.com>
2022-09-21 23:06:04 -0300

    clases 10,11,12,13

---
+++ clase13/js/operaciones.js
@@ -0,0 +1,35 @@
+/*codigo js */
+//comentario en jss
+ function saludar () { //funcion
+     alert (`hola mundo dentro de saludar`);
+ // otras lineas
+ }
+
+ /*operadores*/
+ //cuentas matematicas
+ /* + 
+ -
+ /
+ *
+ */
+/*//variable:
+//sumar dos valores y mostrar resultado
+let op1 =  1;
+let op2 = 2;
+let res = op1 + op2;
+//muestro el resultado por consola
+console.log (res);
+//por alerta
+//alert(res); //muestra cartel en pantalla*/
+
+//variable:
+//sumar dos valores y mostrar resultado
+let op1 = prompt(`ìngrese el valor de op1`); //string o cadena
+//`1``+2 > `12`;
+let op2 = prompt(`ingrese el valor de op2`);
+let res; //declaro variable5
+res = (+op1) + parseInt(op2); //asigno valor
+//muestro el resultado por consola
+console.log (res);
+//por alerta
+alert('resultado=' + res); //muestra cartel en pantalla
